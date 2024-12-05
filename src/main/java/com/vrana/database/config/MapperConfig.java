43cf72020c6be89956a0409af16687e32f2c0a1e
Gitmoid <484635@mail.muni.cz>
2024-12-05 19:54:31 +0100

    Nested authors are created automatically when a book is created

--- src/main/java/com/vrana/database/config/MapperConfig.java
+++ src/main/java/com/vrana/database/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.vrana.database.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,3 +13,11 @@ public class MapperConfig {
         return new ModelMapper();
     }
 }
+
+// guess it handles nested authors automatically now
+//    @Bean
+//    public ModelMapper modelMapper() {
+//        ModelMapper modelMapper = new ModelMapper();
+//        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+//        return modelMapper;
+//    }
