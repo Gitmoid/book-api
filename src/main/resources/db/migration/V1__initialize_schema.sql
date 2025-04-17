16b5f51b04db9fc85cb5abc6576c55de103f5587
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-04-17 20:17:38 +0200

    add flyway and configure it

---
+++ src/main/resources/db/migration/V1__initialize_schema.sql
@@ -0,0 +1,15 @@
+
+CREATE SEQUENCE IF NOT EXISTS author_id_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS authors (
+    id BIGINT PRIMARY KEY DEFAULT NEXT VALUE FOR author_id_seq,
+    name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    isbn VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255),
+    author_id BIGINT,
+    FOREIGN KEY (author_id) REFERENCES authors(id)
+);
