27475c18c95710199f95750eb9be0b76c01bdf84
Gitmoid <vrana.vojtech@post.cz>
2025-04-21 13:35:56 +0200

    add spock and groovy reorganize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.5"
+    groovy
+    id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,23 +25,37 @@ repositories {
 }
 
 dependencies {
+    // Core
     implementation(libs.org.springframework.boot.spring.boot.starter.data.jpa)
     implementation(libs.org.springframework.boot.spring.boot.starter.validation)
     implementation(libs.org.springframework.boot.spring.boot.starter.web)
+
+    // Database
+    implementation(libs.org.flywaydb.flyway.core)
+    runtimeOnly(libs.org.flywaydb.flyway.postgresql)
+    runtimeOnly(libs.org.postgresql.postgresql)
+
+    // Development
+    developmentOnly(libs.org.springframework.boot.spring.boot.devtools)
+    developmentOnly(libs.org.springframework.boot.spring.boot.docker.compose)
+
+    // Tools & Utilities
     compileOnly(libs.org.projectlombok.lombok)
+    annotationProcessor(libs.org.projectlombok.lombok)
+
     implementation(libs.org.mapstruct.mapstruct)
     implementation(libs.org.projectlombok.lombok.mapstruct.binding)
-    implementation(libs.org.flywaydb.flyway.core)
-    runtimeOnly(libs.org.springframework.boot.spring.boot.devtools)
-    runtimeOnly(libs.org.springframework.boot.spring.boot.docker.compose)
-    runtimeOnly(libs.org.postgresql.postgresql)
-    runtimeOnly(libs.org.flywaydb.flyway.postgresql)
-    annotationProcessor(libs.org.projectlombok.lombok)
     annotationProcessor(libs.org.mapstruct.mapstruct.processor)
+
+    // Testing
     testImplementation(libs.org.springframework.boot.spring.boot.starter.test)
-    testImplementation(libs.org.springframework.boot.spring.boot.testcontainers)
+    testImplementation(libs.org.spockframework.spock.core)
+    testImplementation(libs.org.spockframework.spock.spring)
+
+    // Containers
     testImplementation(libs.org.testcontainers.junit.jupiter)
     testImplementation(libs.org.testcontainers.postgresql)
+    testImplementation(libs.org.springframework.boot.spring.boot.testcontainers)
 }
 
 tasks.withType<Test> {
