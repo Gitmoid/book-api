0a58c37919d5244e068dbdf2c0a6ab5e9aabb18b
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-04-25 20:22:23 +0200

    add GET tests to retrieve a list of all authors

--- src/test/groovy/com/vrana/database/controllers/AuthorControllerITSpec.groovy
+++ src/test/groovy/com/vrana/database/controllers/AuthorControllerITSpec.groovy
@@ -2,6 +2,7 @@ package com.vrana.database.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.vrana.database.TestDataUtil
+import com.vrana.database.services.AuthorService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -30,19 +31,21 @@ class AuthorControllerITSpec extends Specification {
     @Autowired
     private ObjectMapper objectMapper
 
+    @Autowired
+    AuthorService authorService
+
     def "connection to postgresql test container established"() {
-        expect:
+        expect: "postgresql test container was created successfully and is running"
         postgres.isCreated()
         postgres.isRunning()
     }
 
     def "CreateAuthor returns HttpStatus 201 CREATED"() {
         given: "a new author DTO"
-        def testAuthorA = TestDataUtil.createTestAuthorEntityA()
-        testAuthorA.setId(null)
+        def testAuthorDtoA = TestDataUtil.createTestAuthorDtoA()
 
         and: "the author DTO as a JSON string"
-        def authorJson = objectMapper.writeValueAsString(testAuthorA)
+        def authorJson = objectMapper.writeValueAsString(testAuthorDtoA)
 
         when: "a POST request is made to create the author"
         def result = mockMvc.perform(
@@ -53,13 +56,41 @@ class AuthorControllerITSpec extends Specification {
 
         then:
         verifyAll {
-            "the response status is CREATED"
+            "the response status is 201 CREATED"
             result.andExpect(MockMvcResultMatchers.status().isCreated())
 
             "the body contains the created author"
             result.andExpect(MockMvcResultMatchers.jsonPath('$.id').isNumber())
-            result.andExpect(MockMvcResultMatchers.jsonPath('$.name').value(testAuthorA.getName()))
-            result.andExpect(MockMvcResultMatchers.jsonPath('$.age').value(testAuthorA.getAge()))
+            result.andExpect(MockMvcResultMatchers.jsonPath('$.name').value(testAuthorDtoA.getName()))
+            result.andExpect(MockMvcResultMatchers.jsonPath('$.age').value(testAuthorDtoA.getAge()))
         }
+    }
+
+    def "ListAuthors returns HttpStatus 200 OK"() {
+        when: "a GET request is made to retrieve all authors"
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+
+        then: "the response status is 200 OK"
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+    }
+
+    def "ListAuthors returns a list of authors"() {
+        given: "a new AuthorEntity saved in the repository"
+        def testAuthorEntityA = TestDataUtil.createTestAuthorEntityA()
+        authorService.saveAuthor(testAuthorEntityA)
+
+        when: "a GET request is made to retrieve all authors"
+        def result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/authors")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+
+        then: "the response is a list of authors with matching author body"
+        result.andExpect(MockMvcResultMatchers.jsonPath('[0].id').isNumber())
+        result.andExpect(MockMvcResultMatchers.jsonPath('[0].name').value(testAuthorEntityA.getName()))
+        result.andExpect(MockMvcResultMatchers.jsonPath('[0].age').value(testAuthorEntityA.getAge()))
     }
 }
