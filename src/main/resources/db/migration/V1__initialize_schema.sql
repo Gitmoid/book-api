1765a927f567b36666e78723bf383429543d2330
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-04-27 21:14:32 +0200

    fixed: add `id` as `PRIMARY KEY` and make `isbn` `UNIQUE`

--- src/main/resources/db/migration/V1__initialize_schema.sql
+++ src/main/resources/db/migration/V1__initialize_schema.sql
@@ -1,15 +1,18 @@
-
 CREATE SEQUENCE author_id_seq START WITH 1 INCREMENT BY 50;
+CREATE SEQUENCE book_id_seq START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE authors (
-    id BIGINT PRIMARY KEY DEFAULT nextval('author_id_seq'),
+CREATE TABLE authors
+(
+    id   BIGINT PRIMARY KEY DEFAULT nextval('author_id_seq'),
     name VARCHAR(255),
-    age INTEGER
+    age  INTEGER
 );
 
-CREATE TABLE books (
-    isbn VARCHAR(255) PRIMARY KEY,
-    title VARCHAR(255),
+CREATE TABLE books
+(
+    id        BIGINT PRIMARY KEY DEFAULT nextval('book_id_seq'),
+    isbn      VARCHAR(255) UNIQUE,
+    title     VARCHAR(255),
     author_id BIGINT,
-    FOREIGN KEY (author_id) REFERENCES authors(id)
+    FOREIGN KEY (author_id) REFERENCES authors (id)
 );
