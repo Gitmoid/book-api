84e4e1a7b1fceb19b0b25b3f3a475f7c02e389ed
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-04-27 21:04:42 +0200

    fixed: remove `IF NOT EXISTS`

--- src/main/resources/db/migration/V1__initialize_schema.sql
+++ src/main/resources/db/migration/V1__initialize_schema.sql
@@ -1,13 +1,13 @@
 
-CREATE SEQUENCE IF NOT EXISTS author_id_seq START WITH 1 INCREMENT BY 50;
+CREATE SEQUENCE author_id_seq START WITH 1 INCREMENT BY 50;
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE authors (
     id BIGINT PRIMARY KEY DEFAULT nextval('author_id_seq'),
     name VARCHAR(255),
     age INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE books (
     isbn VARCHAR(255) PRIMARY KEY,
     title VARCHAR(255),
     author_id BIGINT,
