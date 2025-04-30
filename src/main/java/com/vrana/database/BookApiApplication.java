69d659e86c7c76512cd263700ffb65b3c47210a1
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-04-30 14:03:52 +0200

    fixed: remove `CommandLineRunner`

--- src/main/java/com/vrana/database/BookApiApplication.java
+++ src/main/java/com/vrana/database/BookApiApplication.java
@@ -1,19 +1,14 @@
 package com.vrana.database;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Log
-public class BookApiApplication implements CommandLineRunner {
+public class BookApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
 	}
 }
