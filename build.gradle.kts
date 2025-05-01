cb81a88e86c619a8fb313c759f4b3c01a4b235a1
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-05-01 02:34:43 +0200

    change maven to gradle kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.org.springframework.boot.spring.boot.starter.data.jpa)
+    implementation(libs.org.springframework.boot.spring.boot.starter.validation)
+    implementation(libs.org.springframework.boot.spring.boot.starter.web)
+    compileOnly(libs.org.projectlombok.lombok)
+    implementation(libs.org.mapstruct.mapstruct)
+    implementation(libs.org.projectlombok.lombok.mapstruct.binding)
+    runtimeOnly(libs.org.springframework.boot.spring.boot.devtools)
+    runtimeOnly(libs.org.springframework.boot.spring.boot.docker.compose)
+    runtimeOnly(libs.org.postgresql.postgresql)
+    annotationProcessor(libs.org.projectlombok.lombok)
+    annotationProcessor(libs.org.mapstruct.mapstruct.processor)
+    testImplementation(libs.org.springframework.boot.spring.boot.starter.test)
+    testImplementation(libs.org.springframework.boot.spring.boot.testcontainers)
+    testImplementation(libs.org.testcontainers.junit.jupiter)
+    testImplementation(libs.org.testcontainers.postgresql)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
