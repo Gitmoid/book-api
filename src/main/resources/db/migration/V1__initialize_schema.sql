8a12907f06837130acf47cea452c1a196561c87d
Gitmoid <89934952+Gitmoid@users.noreply.github.com>
2025-05-03 10:54:58 +0200

    fixed: remove sequence from schema change `GenerationType.SEQUENCE` to `IDENTITY`

--- src/main/resources/db/migration/V1__initialize_schema.sql
+++ src/main/resources/db/migration/V1__initialize_schema.sql
@@ -1,16 +1,13 @@
-CREATE SEQUENCE author_id_seq START WITH 1 INCREMENT BY 50;
-CREATE SEQUENCE book_id_seq START WITH 1 INCREMENT BY 1;
-
 CREATE TABLE authors
 (
-    id   BIGINT PRIMARY KEY DEFAULT nextval('author_id_seq'),
+    id   BIGSERIAL PRIMARY KEY,
     name VARCHAR(255),
     age  INTEGER
 );
 
 CREATE TABLE books
 (
-    id        BIGINT PRIMARY KEY DEFAULT nextval('book_id_seq'),
+    id        BIGSERIAL PRIMARY KEY,
     isbn      VARCHAR(255) UNIQUE,
     title     VARCHAR(255),
     author_id BIGINT,
